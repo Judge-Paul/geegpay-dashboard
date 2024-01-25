3d26cb2c506a3c0664a605de370a8225c46c9850
Judge-Paul <judgepaulogebe3@gmail.com>
2024-01-25 11:18:22 +0100

    feat: create mobile sidebar

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -8,7 +8,7 @@ export const Layout = ({ children }: { children: React.ReactNode }) => {
       <Header />
       <div className="flex h-screen border-collapse overflow-hidden">
         <Sidebar />
-        <main className="flex-1 overflow-y-auto overflow-x-hidden pt-16 bg-secondary/10 pb-1">
+        <main className="flex-1 overflow-y-auto overflow-x-hidden pt-16 bg-background pb-1">
           {children}
         </main>
       </div>
