57cc28668cea283b06ec88c193d650f8ddd629ea
Judge-Paul <judgepaulogebe3@gmail.com>
2024-01-26 05:55:18 +0100

    fix: clear unused imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,5 @@
-import Image from "next/image";
 import { Inter } from "next/font/google";
-import { Button } from "@/components/ui/button";
-import { ModeToggle } from "@/components/ModeToggle";
 import { Layout } from "@/components/layout";
-import VerticalToggle from "@/components/Toggle";
 
 const inter = Inter({ subsets: ["latin"] });
 
