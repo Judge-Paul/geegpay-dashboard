d90519acbae705b3e4d369fe1a746ad363d4cefa
Judge-Paul <judgepaulogebe3@gmail.com>
2024-01-26 23:17:21 +0100

    feat: create helper function for creating bar chart data

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,68 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+interface chartData {
+  name: string;
+  total: number;
+}
+
+export function getMonthlyData(): chartData[] {
+  const currentDate = new Date();
+  const result = [];
+
+  for (let i = 0; i < 12; i++) {
+    const newDate = new Date(currentDate);
+    newDate.setMonth(currentDate.getMonth() - i);
+
+    const month = newDate.toLocaleString("default", { month: "short" });
+    const year = newDate.getFullYear().toString();
+    const formattedMonth = `${month} ${year[2]}${year[3]}`;
+
+    const total = Math.floor(Math.random() * 40000) + 10000;
+    result.push({ name: formattedMonth, total });
+  }
+
+  return result.reverse();
+}
+
+export function getWeeklyData(): chartData[] {
+  const currentDate = new Date();
+  const result = [];
+
+  for (let i = 0; i < 12; i++) {
+    const newDate = new Date(currentDate);
+    newDate.setDate(currentDate.getDate() - i * 7);
+
+    const month = newDate.toLocaleString("default", { month: "short" });
+    const day = newDate.getDate();
+    const formattedDate = `${month} ${day}`;
+
+    const total = Math.floor(Math.random() * 10000) + 2000;
+    result.push({ name: formattedDate, total });
+  }
+
+  return result.reverse();
+}
+
+export function getDailyData(): chartData[] {
+  const currentDate = new Date();
+  const result = [];
+
+  for (let i = 0; i < 12; i++) {
+    const newDate = new Date(currentDate);
+    newDate.setDate(currentDate.getDate() - i);
+
+    const month = newDate.toLocaleString("default", { month: "short" });
+    const day = newDate.getDate();
+    const formattedDate = `${month} ${day}`;
+
+    const total = Math.floor(Math.random() * 1300) + 50;
+    result.push({ name: formattedDate, total });
+  }
+
+  return result.reverse();
 }
