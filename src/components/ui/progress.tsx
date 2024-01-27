e625ecde1af20fb46027c5d6b17b236186c12619
Judge-Paul <judgepaulogebe3@gmail.com>
2024-01-27 15:37:24 +0100

    fix: update progress props in Progress component

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -3,9 +3,13 @@ import * as ProgressPrimitive from "@radix-ui/react-progress";
 
 import { cn } from "@/lib/utils";
 
+interface ExtraProps {
+  barClassName?: string;
+}
+
 const Progress = React.forwardRef<
   React.ElementRef<typeof ProgressPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
+  React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root> & ExtraProps
 >(({ className, value, barClassName, ...props }, ref) => (
   <ProgressPrimitive.Root
     ref={ref}
