9aa03828750c669a056008f5d54e9c10db3be55d
Judge-Paul <judgepaulogebe3@gmail.com>
2024-01-27 16:10:16 +0100

    chore: cleanup

--- src/components/dashboard/AreaChart.tsx
+++ src/components/dashboard/AreaChart.tsx
@@ -10,9 +10,6 @@ interface ChartProps {
 export default function Chart({ data, stroke, fill }: ChartProps) {
   return (
     <ResponsiveContainer width="100%" height="100%">
-      {/* <LineChart width={500} height={300} data={data}>
-        <Line type="monotone" dot={false} dataKey="value" stroke="#82ca9d" />
-      </LineChart> */}
       <AreaChart data={data}>
         <Area type="monotone" dataKey="value" stroke={stroke} fill={fill} />
       </AreaChart>
